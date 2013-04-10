f3bae4ac7862ba767f9a5a1504649dde24beb8be
Victoria Gan <glitz_gem@hotmail.com>
2013-04-11 02:51:38 +0800

    Map for house, church, cc, mall
    havent drawn special NPCs yet

--- data/objects.js
+++ data/objects.js
@@ -90,5 +90,166 @@ var objects =
 		"src":"images/cc.png",
 		"width":"30",
 		"height":"30"
+	},
+	"13":{
+		"id":"f",	//closet
+		"type":"scenery",
+		"src":"images/closet.png",
+		"width":"32",
+		"height":"32"
+	},
+	"14":{
+		"id":"g",	//grass
+		"type":"scenery",
+		"src":"images/grass.png",
+		"width":"32",
+		"height":"32"
+	},
+	"15":{
+		"id":"h",	//cross
+		"type":"scenery",
+		"src":"images/cross.png",
+		"width":"32",
+		"height":"32"
+	},
+	"16":{
+		"id":"i",	//drums
+		"type":"scenery",
+		"src":"images/drums.png",
+		"width":"32",
+		"height":"32"
+	},
+	"17":{
+		"id":"j",	//radio
+		"type":"scenery",
+		"src":"images/radio.png",
+		"width":"32",
+		"height":"32"
+	},
+	"18":{
+		"id":"k",	//sink
+		"type":"scenery",
+		"src":"images/sink.png",
+		"width":"32",
+		"height":"32"
+	},
+	"19":{
+		"id":"l",	//pulpit
+		"type":"scenery",
+		"src":"images/pulpit.png",
+		"width":"32",
+		"height":"32"
+	},
+	"20":{
+		"id":"o",	//toys
+		"type":"scenery",
+		"src":"images/toys.png",
+		"width":"32",
+		"height":"32"
+	},
+	"21":{
+		"id":"p",	//plant
+		"type":"scenery",
+		"src":"images/plant.png",
+		"width":"32",
+		"height":"32"
+	},
+	"22":{
+		"id":"q",	//piano
+		"type":"scenery",
+		"src":"images/piano.png",
+		"width":"32",
+		"height":"32"
+	},
+	"23":{
+		"id":"r",	//refrigerator
+		"type":"scenery",
+		"src":"images/refrigerator.png",
+		"width":"32",
+		"height":"32"
+	},
+	"24":{
+		"id":"t",	//toilet bowl
+		"type":"scenery",
+		"src":"images/toiletBowl.png",
+		"width":"32",
+		"height":"32"
+	},
+	"25":{
+		"id":"u",	//stove
+		"type":"scenery",
+		"src":"images/stove.png",
+		"width":"32",
+		"height":"32"
+	},
+	"26":{
+		"id":"v",	//TV
+		"type":"scenery",
+		"src":"images/tv.png",
+		"width":"32",
+		"height":"32"
+	},
+	"27":{
+		"id":"w",	//women clothes
+		"type":"scenery",
+		"src":"images/womenClothes.png",
+		"width":"32",
+		"height":"32"
+	},
+	"28":{
+		"id":"x",	//chair(L)
+		"type":"scenery",
+		"src":"images/chair(L).png",
+		"width":"32",
+		"height":"32"
+	},
+	"29":{
+		"id":"y",	//chair(R)
+		"type":"scenery",
+		"src":"images/chair(R).png",
+		"width":"32",
+		"height":"32"
+	},
+	"30":{
+		"id":"z",	//chair(B)
+		"type":"scenery",
+		"src":"images/chair(B).png",
+		"width":"32",
+		"height":"32"
+	},
+	"31":{
+		"id":"B",	//bed
+		"type":"scenery",
+		"src":"images/bed.png",
+		"width":"32",
+		"height":"32"
+	},
+	"32":{
+		"id":"C",	//cash register
+		"type":"scenery",
+		"src":"images/cashRegister.png",
+		"width":"32",
+		"height":"32"
+	},
+	"33":{
+		"id":"M",	//men clothes
+		"type":"scenery",
+		"src":"images/menClothes.png",
+		"width":"32",
+		"height":"32"
+	},
+	"34":{
+		"id":"S",	//shoes
+		"type":"scenery",
+		"src":"images/shoes.png",
+		"width":"32",
+		"height":"32"
+	},
+	"35":{
+		"id":"T",	//table
+		"type":"scenery",
+		"src":"images/table.png",
+		"width":"32",
+		"height":"32"
 	}
 }
